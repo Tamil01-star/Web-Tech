9f0d99a26e7f5f8f42d68e17a562deb150a8f367
Tamil01-star <tamil01star@github.com>
2026-08-07 20:08:57 +0530

    ✨ Tech Links: Added Quartz Components to Kart Section

--- js/data.js
+++ js/data.js
@@ -827,7 +827,8 @@ var TECH_LINKS = [
       { name: 'Robocraze', url: 'https://robocraze.com/', desc: 'Online electronics components and robotics store.' },
       { name: 'Majestronicz', url: 'https://majestronicz.in/', desc: 'Electronic components and DIY kits store.' },
       { name: 'Swastik Kits', url: 'https://www.swastikkits.com/', desc: 'Electronic project kits and components.' },
-      { name: 'Robu.in', url: 'https://robu.in/', desc: 'Leading electronics, robotics, and drone components store in India.' }
+      { name: 'Robu.in', url: 'https://robu.in/', desc: 'Leading electronics, robotics, and drone components store in India.' },
+      { name: 'Quartz Components', url: 'https://quartzcomponents.com/collections/fnirsi', desc: 'Electronic components and testing equipment (FNIRSI).' }
     ]
   },
   {
